6d2e984c5b6f1ae631a0001e7c623e2bc39c5801
Mirko Kiefer <mail@mirkokiefer.com>
2013-08-07 00:57:32 +0200

    test with large file

--- test/index.js
+++ test/index.js
@@ -4,15 +4,37 @@ var fs = require('fs')
 var createLineIterator = require('../lib/index')
 var createStreamIterator = require('stream-iterator')
 var iterators = require('async-iterators')
+var async = require('async')
+
+var testData = []
+for (var i=0; i<10000; i++) {
+  testData.push('this is line ' + i)
+}
+
+var testFile = __dirname + '/test.txt'
+var writeTestFile = function(cb) {
+  var writeStream = fs.createWriteStream(testFile)
+  async.forEach(testData, function(each, cb) {
+    writeStream.write(each + '\n', cb)
+  }, function() {
+    writeStream.close()
+    cb()
+  })
+}
+
 
 describe('line-stream', function() {
+  before(writeTestFile)
+  after(function(done) {
+    fs.unlink(testFile, done)
+  })
   it('should read from a character stream and transform to a line stream', function(done) {
-    var fileStream = fs.createReadStream(__dirname + '/test.txt', {encoding: 'utf8'})
+    var fileStream = fs.createReadStream(testFile, {encoding: 'utf8'})
     var fileIterator = createStreamIterator(fileStream)
     var lineIterator = createLineIterator(fileIterator)
-    console.log('ok')
     iterators.map(lineIterator, function(err, each) { return each }, function(err, res) {
-      var expectedRes = ["line one","line two","line three"]
+      assert.equal(res.length, 10001)
+      var expectedRes = testData.concat([''])
       assert.deepEqual(res, expectedRes)
       done()
     })
